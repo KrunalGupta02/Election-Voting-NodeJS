d3cac1e22407c11b2b9fe04ff1317ac9ebc32dd8
Krunal Gupta <guptakrunal02@gmail.com>
2025-04-21 23:56:02 +0530

    Added models and install required dependencies

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json()) // req.body
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listeing on port ${PORT}`);
+})
